7f2326f6f1f7d89ec9f398a7352d31caecb56239
Jonathan <bobchu351@gmail.com>
2020-10-17 15:52:07 -0700

    added MessageEventListener

---
+++ src/main/java/PingPackage/MessageEventListener.java
@@ -0,0 +1,11 @@
+package PingPackage;
+
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.jetbrains.annotations.NotNull;
+
+public class MessageEventListener extends ListenerAdapter {
+    public void onMessageReceived(@NotNull MessageReceivedEvent event) {
+
+    }
+}
--- src/main/java/PingPackage/PingBotMain.java
+++ src/main/java/PingPackage/PingBotMain.java
@@ -2,22 +2,14 @@ package PingPackage;
 
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
-import net.dv8tion.jda.api.events.GenericEvent;
-import net.dv8tion.jda.api.hooks.EventListener;
-
-import javax.annotation.Nonnull;
 import javax.security.auth.login.LoginException;
 
-public class PingBotMain implements EventListener {
+public class PingBotMain {
     public static void main(String[] args) throws LoginException {
         JDA jda = JDABuilder
-            .createDefault("NzY3MTUyMTE1ODE0MTA1MTE5.X4twFQ.x6xCy1L_aJVyXrKREiizlVYd42I")
-            .addEventListeners(new PingBotMain())
-            .build();
+                .createDefault("NzY3MTUyMTE1ODE0MTA1MTE5.X4twFQ.x6xCy1L_aJVyXrKREiizlVYd42I")
+                .addEventListeners(new MessageEventListener())
+                .build();
         System.out.println(jda.getInviteUrl());
-    }
-    @Override
-    public void onEvent(@Nonnull GenericEvent event) {
-
     }
 }
