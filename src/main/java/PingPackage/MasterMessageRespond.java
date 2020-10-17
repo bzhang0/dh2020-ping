518c95c3f5d3ce1433fdbe43a38b9e5af176398a
therealgalaxi <42699471+therealgalaxi@users.noreply.github.com>
2020-10-17 16:57:53 -0700

    reaction and !ping

--- src/main/java/PingPackage/MasterMessageRespond.java
+++ src/main/java/PingPackage/MasterMessageRespond.java
@@ -1,11 +1,28 @@
 package PingPackage;
 
 import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
-public class MasterMessageRespond {
+public class MasterMessageRespond extends ListenerAdapter {
+
+
+    @Override
     public void onGuildMessageReceived(GuildMessageReceivedEvent e) {
-        Message messageMessage = e.getMessage();
-        String message = e.getMessage().getContentRaw();
+        Message message = e.getMessage();
+        TextChannel channel = e.getChannel();
+        String msg = message.getContentRaw();
+
+        System.out.println("Message sent by " + e.getAuthor().getName() + " [" +
+                e.getAuthor().getId() + "]: " + message.getContentRaw());
+
+        if (msg.equals("!ping")) {
+            channel.sendMessage("pong!").complete();
+        }
+
+        if (msg.contains("play")) {
+            channel.addReactionById(message.getId(),"\uD83C\uDFAE").complete();
+        }
     }
 }
--- src/main/java/PingPackage/PingBotMain.java
+++ src/main/java/PingPackage/PingBotMain.java
@@ -1,23 +1,21 @@
 package PingPackage;
 
+import jdk.incubator.http.WebSocket;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.events.GenericEvent;
 import net.dv8tion.jda.api.hooks.EventListener;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 import javax.annotation.Nonnull;
 import javax.security.auth.login.LoginException;
 
-public class PingBotMain implements EventListener {
+public class PingBotMain extends ListenerAdapter {
     public static void main(String[] args) throws LoginException {
         JDA jda = JDABuilder
             .createDefault("NzY3MTUyMTE1ODE0MTA1MTE5.X4twFQ.x6xCy1L_aJVyXrKREiizlVYd42I")
-            .addEventListeners(new PingBotMain())
+            .addEventListeners(new PingBotMain(), new MasterMessageRespond())
             .build();
         System.out.println(jda.getInviteUrl());
-    }
-    @Override
-    public void onEvent(@Nonnull GenericEvent event) {
-
     }
 }
