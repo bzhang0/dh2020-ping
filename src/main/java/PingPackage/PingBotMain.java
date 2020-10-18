3ea20f9f525f9f892b5ea611ccfb9309ce6d4b99
Jonathan <bobchu351@gmail.com>
2020-10-17 17:25:49 -0700

    added Steam API Parser

--- src/main/java/PingPackage/PingBotMain.java
+++ src/main/java/PingPackage/PingBotMain.java
@@ -7,8 +7,13 @@ import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 import javax.security.auth.login.LoginException;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
 
-public class PingBotMain {
+public class PingBotMain extends ListenerAdapter {
     public static void main(String[] args) throws LoginException {
         JDA jda = JDABuilder
                 .createDefault("NzY3MTUyMTE1ODE0MTA1MTE5.X4twFQ.x6xCy1L_aJVyXrKREiizlVYd42I")
@@ -16,7 +21,56 @@ public class PingBotMain {
                         new PingBotMain(),
                         new MasterMessageRespond())
                 .build();
+        try {
+            readSteamAPI();
+        } catch (Exception e) {
+            System.out.println("Failed reading Steam API");
+            e.printStackTrace();
+        }
         System.out.println(jda.getInviteUrl());
+    }
+    // Takes an input file and checks if it exists
+    // Promptly returns it to another method for later use
+    private static File identifyFile() {
+        try {
+            File inputFile = new File("inputFile.txt");
+            if (inputFile.createNewFile()) {
+                System.out.println("Debug : File Created");
+            } else {
+                System.out.println("Debug : File Exists");
+            }
+            return inputFile;
+        } catch (IOException e) {
+            System.out.println("Error reading input");
+            e.printStackTrace();
+        }
+        return null;
+    }
+    // Reads the Steam API given by Gabe using BufferedReader
+    // Method includes parser to indicate name of owned games with subsequent play times (in minutes)
+    private static void readSteamAPI() throws IOException {
+        URL oracle = new URL("http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=407CEDDFFC32EE02E865328245752A79&steamid=76561198129178478&include_appinfo=true&format=xml");
+        BufferedReader in = new BufferedReader(new InputStreamReader(oracle.openStream()));
+        String inputLine;
+        while ((inputLine = in.readLine()) != null){
+            if (inputLine.contains("<name>")) {
+                String name;
+                name = inputLine.replaceAll("<name>", "");
+                name = name.replaceAll("</name>", "");
+                if (name.contains("&apos;"))
+                    name = name.replaceAll("&apos;", "'");
+                System.out.println(name.trim());
+            }
+            if (inputLine.contains("<playtime_forever>")) {
+                String playtime;
+                playtime = inputLine.replaceAll("<playtime_forever>", "");
+                playtime = playtime.replaceAll("</playtime_forever>", "");
+                if (playtime.contains("&apos;"))
+                    playtime = playtime.replaceAll("&apos;", "'");
+                System.out.println(playtime.trim());
+            }
+        }
+        in.close();
     }
 }
 class MasterMessageRespond extends ListenerAdapter {
