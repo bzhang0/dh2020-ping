f4531bea092e17dc2e2574de8c509ded0eecab06
therealgalaxi <42699471+therealgalaxi@users.noreply.github.com>
2020-10-17 17:33:46 -0700

    Revert "embed 1.0"
    This reverts commit f35a31091b9a6e91987762459ed17fd9aea838db.

--- src/main/java/PingPackage/MasterMessageRespond.java
+++ src/main/java/PingPackage/MasterMessageRespond.java
@@ -1,16 +1,12 @@
 package PingPackage;
 
-import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
-import java.util.EmptyStackException;
-
 public class MasterMessageRespond extends ListenerAdapter {
 
-    private String PREFIX = ".";
 
     @Override
     public void onGuildMessageReceived(GuildMessageReceivedEvent e) {
@@ -21,26 +17,12 @@ public class MasterMessageRespond extends ListenerAdapter {
         System.out.println("Message sent by " + e.getAuthor().getName() + " [" +
                 e.getAuthor().getId() + "]: " + message.getContentRaw());
 
-        if (msg.equals(".ping")) {
+        if (msg.equals("!ping")) {
             channel.sendMessage("pong!").complete();
         }
 
         if (msg.contains("play")) {
             channel.addReactionById(message.getId(),"\uD83C\uDFAE").complete();
         }
-
-        if (msg.equals(".help")) {
-            help(e);
-        }
-    }
-
-    public void help(GuildMessageReceivedEvent e) {
-        EmbedBuilder embed = new EmbedBuilder();
-
-        embed.setDescription("commands are summoned by " + PREFIX + "<command>");
-
-        embed.addField(PREFIX + "blabla", "HI GUBA", false);
-
-        e.getChannel().sendMessage(MasterMessageAction.defaultEmbed(embed, "help", PREFIX).build()).complete();
     }
 }
