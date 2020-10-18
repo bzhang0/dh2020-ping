acf73e73ca5697d40956ec9c32c1d73e6349599a
Ben Zhang <42699471+therealgalaxi@users.noreply.github.com>
2020-10-17 18:19:57 -0700

    yeh

---
+++ src/main/java/PingPackage/MasterMessageRespond.java
@@ -0,0 +1,65 @@
+package PingPackage;
+
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.*;
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import java.util.Scanner;
+
+public class MasterMessageRespond extends ListenerAdapter {
+
+    private String PREFIX = ".";
+
+    @Override
+    public void onGuildMessageReceived(GuildMessageReceivedEvent e) {
+        messageLog(e);
+
+        User author = e.getAuthor();
+        Member member = e.getMember();
+
+        Message message = e.getMessage();
+        String msg = message.getContentRaw();
+
+        TextChannel channel = e.getChannel();
+
+        if (msg.length() > PREFIX.length() && msg.startsWith(PREFIX) && !e.getAuthor().isBot()) {
+            long startTime = System.nanoTime();
+            Scanner sentMessageInput = new Scanner(msg.substring(PREFIX.length()));
+
+            String authorID = author.getId();
+            String prompt = sentMessageInput.next();
+//            e.getMember().get
+
+//            if (prompt.equalsIgnoreCase("quit") && ()) {
+//
+//            }
+        }
+        if (msg.equals(".ping")) {
+            channel.sendMessage("pong!").complete();
+        }
+
+        if (msg.contains("play")) {
+            channel.addReactionById(message.getId(), "\uD83C\uDFAE").complete();
+        }
+
+        if (msg.equals(".help")) {
+            help(e);
+        }
+    }
+
+    public void help(GuildMessageReceivedEvent e) {
+        EmbedBuilder embed = new EmbedBuilder();
+
+        embed.setDescription("commands are summoned by " + PREFIX + "<command>");
+
+        embed.addField(PREFIX + "blabla", "HI GUBA", false);
+
+        e.getChannel().sendMessage(MasterMessageAction.defaultEmbed(embed, "help", PREFIX).build()).complete();
+    }
+
+    public void messageLog(GuildMessageReceivedEvent e) {
+        System.out.println("Message in " + e.getGuild().getName() + " sent by " +
+                e.getAuthor().getName() + " in " + e.getChannel().getName() +
+                ": " + e.getMessage().getContentRaw() + " ");
+    }
+}
--- src/main/java/PingPackage/PingBotMain.java
+++ src/main/java/PingPackage/PingBotMain.java
@@ -2,22 +2,65 @@ package PingPackage;
 
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
-import net.dv8tion.jda.api.events.GenericEvent;
-import net.dv8tion.jda.api.hooks.EventListener;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
-import javax.annotation.Nonnull;
 import javax.security.auth.login.LoginException;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
 
-public class PingBotMain implements EventListener {
+public class PingBotMain extends ListenerAdapter {
     public static void main(String[] args) throws LoginException {
         JDA jda = JDABuilder
             .createDefault("NzY3MTUyMTE1ODE0MTA1MTE5.X4twFQ.x6xCy1L_aJVyXrKREiizlVYd42I")
-            .addEventListeners(new PingBotMain())
+            .addEventListeners(new PingBotMain(), new MasterMessageRespond())
             .build();
         System.out.println(jda.getInviteUrl());
     }
-    @Override
-    public void onEvent(@Nonnull GenericEvent event) {
-        
+
+    // Takes an input file and checks if it exists
+    // Promptly returns it to another method for later use
+    private static File identifyFile() {
+        try {
+            File inputFile = new File("inputFile.txt");
+            if (inputFile.createNewFile()) {
+                System.out.println("Debug : File Created");
+            } else {
+                System.out.println("Debug : File Exists");
+            }
+            return inputFile;
+        } catch (IOException e) {
+            System.out.println("Error reading input");
+            e.printStackTrace();
+        }
+        return null;
+    }
+    // Reads the Steam API given by Gabe using BufferedReader
+    // Method includes parser to indicate name of owned games with subsequent play times (in minutes)
+    private static void readSteamAPI() throws IOException {
+        URL oracle = new URL("http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=407CEDDFFC32EE02E865328245752A79&steamid=76561198129178478&include_appinfo=true&format=xml");
+        BufferedReader in = new BufferedReader(new InputStreamReader(oracle.openStream()));
+        String inputLine;
+        while ((inputLine = in.readLine()) != null){
+            if (inputLine.contains("<name>")) {
+                String name;
+                name = inputLine.replaceAll("<name>", "");
+                name = name.replaceAll("</name>", "");
+                if (name.contains("&apos;"))
+                    name = name.replaceAll("&apos;", "'");
+                System.out.println(name.trim());
+            }
+            if (inputLine.contains("<playtime_forever>")) {
+                String playtime;
+                playtime = inputLine.replaceAll("<playtime_forever>", "");
+                playtime = playtime.replaceAll("</playtime_forever>", "");
+                if (playtime.contains("&apos;"))
+                    playtime = playtime.replaceAll("&apos;", "'");
+                System.out.println(playtime.trim());
+            }
+        }
+        in.close();
     }
 }
