2248716fd51df408b9cded84771716cf199e4078
therealgalaxi <42699471+therealgalaxi@users.noreply.github.com>
2020-10-17 19:28:40 -0700

    more organization

--- src/main/java/PingPackage/MasterMessageRespond.java
+++ src/main/java/PingPackage/MasterMessageRespond.java
@@ -1,9 +1,13 @@
 package PingPackage;
 
 import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.*;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+import java.util.EnumSet;
+import java.util.List;
 import java.util.Scanner;
 
 public class MasterMessageRespond extends ListenerAdapter {
@@ -14,7 +18,7 @@ public class MasterMessageRespond extends ListenerAdapter {
     public void onGuildMessageReceived(GuildMessageReceivedEvent e) {
         messageLog(e);
 
-        User author = e.getAuthor();
+//        User author = e.getAuthor();
         Member member = e.getMember();
 
         Message message = e.getMessage();
@@ -26,40 +30,56 @@ public class MasterMessageRespond extends ListenerAdapter {
             long startTime = System.nanoTime();
             Scanner sentMessageInput = new Scanner(msg.substring(PREFIX.length()));
 
-            String authorID = author.getId();
             String prompt = sentMessageInput.next();
-//            e.getMember().get
+//            List<Role> roles = e.getMember().getRoles();
 
-//            if (prompt.equalsIgnoreCase("quit") && ()) {
+//            for (Role r : roles) {
+//                EnumSet<Permission> p = r.getPermissions();
 //
+//                if (p.contains(Permission.ADMINISTRATOR)) {
+//                    System.out.println("YEAHHHH");
+//                }
+//                System.out.println(r + " " + p);
 //            }
-        }
-        if (msg.equals(".ping")) {
-            channel.sendMessage("pong!").complete();
+
+            if (prompt.equalsIgnoreCase("quit") && member.getPermissions().contains(Permission.ADMINISTRATOR)) {
+                // save data
+                channel.sendMessage("shutting down...").complete();
+                System.exit(1);
+            } else if (prompt.equalsIgnoreCase("version")) {
+                channel.sendMessage(PingBotMain.VERSION).complete();
+            } else if (prompt.equalsIgnoreCase("ping")) {
+                EmbedBuilder embed = new EmbedBuilder();
+
+                String programTimeString = ("" + ((double)(System.nanoTime() - startTime)) / 1000000.0).substring(0, 5);
+                embed.setTitle("pong!").setFooter("returned in " + programTimeString + "ms");
+//                channel.sendMessage("pong!");
+
+                channel.sendMessage(embed.build()).complete();
+            } else if (prompt.equalsIgnoreCase("help")) {
+                help(e);
+            }
         }
 
+        msg = msg.toLowerCase();
         if (msg.contains("play")) {
             channel.addReactionById(message.getId(), "\uD83C\uDFAE").complete();
-        }
-
-        if (msg.equals(".help")) {
-            help(e);
         }
     }
 
     public void help(GuildMessageReceivedEvent e) {
         EmbedBuilder embed = new EmbedBuilder();
 
-        embed.setDescription("commands are summoned by " + PREFIX + "<command>");
-
-        embed.addField(PREFIX + "blabla", "HI GUBA", false);
+        embed.setDescription("commands are summoned by " + PREFIX + "<command>")
+                .addField(PREFIX + "quit", "shuts down the bot", false)
+                .addField(PREFIX + "ping", "returns pong!", false);
 
         e.getChannel().sendMessage(MasterMessageAction.defaultEmbed(embed, "help", PREFIX).build()).complete();
     }
 
     public void messageLog(GuildMessageReceivedEvent e) {
         System.out.println("Message in " + e.getGuild().getName() + " sent by " +
-                e.getAuthor().getName() + " in " + e.getChannel().getName() +
+                e.getAuthor().getName() + " [" + e.getAuthor().getId() + "] in " + e.getChannel().getName() +
                 ": " + e.getMessage().getContentRaw() + " ");
     }
 }
--- src/main/java/PingPackage/PingBotMain.java
+++ src/main/java/PingPackage/PingBotMain.java
@@ -12,6 +12,9 @@ import java.io.InputStreamReader;
 import java.net.URL;
 
 public class PingBotMain extends ListenerAdapter {
+
+    public static final String VERSION = "1.0";
+
     public static void main(String[] args) throws LoginException {
         JDA jda = JDABuilder
             .createDefault("NzY3MTUyMTE1ODE0MTA1MTE5.X4twFQ.x6xCy1L_aJVyXrKREiizlVYd42I")
@@ -37,6 +40,7 @@ public class PingBotMain extends ListenerAdapter {
         }
         return null;
     }
+
     // Reads the Steam API given by Gabe using BufferedReader
     // Method includes parser to indicate name of owned games with subsequent play times (in minutes)
     private static void readSteamAPI() throws IOException {
