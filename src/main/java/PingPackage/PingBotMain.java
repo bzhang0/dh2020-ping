49d3aabf1654986d876bf25abedcd32abf53bfed
Jonathan <bobchu351@gmail.com>
2020-10-17 18:08:57 -0700

    added !connectsteam

--- src/main/java/PingPackage/PingBotMain.java
+++ src/main/java/PingPackage/PingBotMain.java
@@ -14,6 +14,7 @@ import java.io.InputStreamReader;
 import java.net.URL;
 
 public class PingBotMain extends ListenerAdapter {
+    private static String tempSteamID;
     public static void main(String[] args) throws LoginException {
         JDA jda = JDABuilder
                 .createDefault("NzY3MTUyMTE1ODE0MTA1MTE5.X4twFQ.x6xCy1L_aJVyXrKREiizlVYd42I")
@@ -49,10 +50,13 @@ public class PingBotMain extends ListenerAdapter {
     // Reads the Steam API given by Gabe using BufferedReader
     // Method includes parser to indicate name of owned games with subsequent play times (in minutes)
     private static void readSteamAPI() throws IOException {
-        URL oracle = new URL("http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=407CEDDFFC32EE02E865328245752A79&steamid=76561198129178478&include_appinfo=true&format=xml");
+        URL oracle = new URL("http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=407CEDDFFC32EE02E865328245752A79&steamid="
+                + tempSteamID
+                + "&include_appinfo=true&format=xml");
         BufferedReader in = new BufferedReader(new InputStreamReader(oracle.openStream()));
         String inputLine;
         while ((inputLine = in.readLine()) != null){
+            System.out.println(inputLine);
             if (inputLine.contains("<name>")) {
                 String name;
                 name = inputLine.replaceAll("<name>", "");
@@ -72,6 +76,17 @@ public class PingBotMain extends ListenerAdapter {
         }
         in.close();
     }
+    public static boolean grabSteamID(String id) {
+        tempSteamID = id;
+        try {
+            readSteamAPI();
+            return true;
+        } catch (Exception e) {
+            System.out.println("Invalid ID");
+            e.printStackTrace();
+            return false;
+        }
+    }
 }
 class MasterMessageRespond extends ListenerAdapter {
     @Override
@@ -80,6 +95,13 @@ class MasterMessageRespond extends ListenerAdapter {
         String message = e.getMessage().getContentRaw();
         if (message.equals("!ping")) {
             e.getChannel().sendMessage("pong!").complete();
+        }
+        if (message.startsWith("!connectsteam")) {
+            message = message.replaceAll("!connectsteam", "");
+            message = message.trim();
+            if (PingBotMain.grabSteamID(message)) {
+                e.getChannel().sendMessage("Invalid SteamID").complete();
+            }
         }
     }
 }
